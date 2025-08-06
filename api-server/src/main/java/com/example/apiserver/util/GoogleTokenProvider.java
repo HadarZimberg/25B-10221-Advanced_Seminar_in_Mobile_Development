4906caea5c2f4c9a1381a43d64a04c0a436fc1d6
hadar <hadarzim@gmail.com>
2025-08-06 15:04:23 +0300

    GoogleTokenProvider

--- api-server/src/main/java/com/example/apiserver/util/GoogleTokenProvider.java
+++ api-server/src/main/java/com/example/apiserver/util/GoogleTokenProvider.java
@@ -5,14 +5,24 @@ import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.List;
 
 @Component
 public class GoogleTokenProvider {
 
-	public String getAccessToken() throws IOException {
-        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
-            .createScoped("https://www.googleapis.com/auth/cloud-platform");
+	public static String getAccessToken() throws IOException {
+        String firebaseConfig = System.getenv("FIREBASE_CONFIG_JSON");
+
+        if (firebaseConfig == null || firebaseConfig.isEmpty()) {
+            throw new IOException("FIREBASE_CONFIG_JSON is not set");
+        }
+
+        InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes(StandardCharsets.UTF_8));
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount)
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
 
         credentials.refreshIfExpired();
         return credentials.getAccessToken().getTokenValue();
